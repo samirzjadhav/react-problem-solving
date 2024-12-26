16bb485913905d1bb905de29677d75f9ab4c9b41
samir jadhav <samirzjadhav@gmail.com>
2024-12-26 20:10:32 +0530

    1st beginner level problem solve

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import React from "react";
+import Problem1 from "./BeginnerProblem/Problem1";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Problem1 />
+    </div>
+  );
 }
 
 export default App;
